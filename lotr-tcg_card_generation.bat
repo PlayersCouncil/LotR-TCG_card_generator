9c9e6f3c12da62012f1794adc2cd24e2110afe24
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2020-06-27 18:55:38 -0500

    Adding creation bat for even easier usage

---
+++ lotr-tcg_card_generation.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Running nanDECK...
+
+
+
+nanDECK\1.25.2\nanDECK .\lotr-tcg_virtual_cards.nde /exec
+
+
+
+echo nanDECK card generation complete.  Check your output folder for details.
+pause
+@echo on
